b90fa074dc1d5f8d313b8ce867ac0ebd45d2b455
tazourm-code <tazourm@gmail.com>
2026-04-02 20:36:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Tech VJ'
-
+    return 'Tech VJ - Bot is Running'
 
 if __name__ == "__main__":
-    app.run()
+    # রেন্ডারের পোর্ট অটোমেটিক ডিটেক্ট করার জন্য এটি দরকার
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+    
