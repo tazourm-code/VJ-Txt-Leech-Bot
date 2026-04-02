64166394ebeef7452e3827c0a457c26c6a55cf65
tazourm-code <tazourm@gmail.com>
2026-04-02 20:51:15 +0600

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,10 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import os
 
-# আপনার দেওয়া আইডিগুলো এখানে সেট করা হয়েছে
 API_ID = int(os.environ.get("API_ID", "32681138"))
 API_HASH = os.environ.get("API_HASH", "c809aa4537888310e0f29e49afe13466")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8752628916:AAGeJwdqtWIuwZImPK_H6VwEDuJwgdOqTDw")
