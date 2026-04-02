c26c6f6ae813fabf0dbcfa634ebbf7893eacd84c
tazourm-code <tazourm@gmail.com>
2026-04-02 16:53:16 +0600

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import os
 
 API_ID = 32681138
 API_HASH = "c809aa4537888310e0f29e49afe13466"
-BOT_TOKEN = "8788515111:AAErcQOSagccTugOkO1JBAOFHM9TyaB_V3k"
+BOT_TOKEN = "8752628916:AAGeJwdqtWIuwZImPK_H6VwEDuJwgdOqTDw"
