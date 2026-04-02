1750073a91cf9f8711e51cd73f8099993910cb13
tazourm-code <tazourm@gmail.com>
2026-04-02 20:11:42 +0600

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,7 @@
 
 import os
 
-API_ID = 32681138
-API_HASH = "c809aa4537888310e0f29e49afe13466"
-BOT_TOKEN = "8752628916:AAGeJwdqtWIuwZImPK_H6VwEDuJwgdOqTDw"
+# আপনার দেওয়া আইডিগুলো এখানে সেট করা হয়েছে
+API_ID = int(os.environ.get("API_ID", "32681138"))
+API_HASH = os.environ.get("API_HASH", "c809aa4537888310e0f29e49afe13466")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8752628916:AAGeJwdqtWIuwZImPK_H6VwEDuJwgdOqTDw")
